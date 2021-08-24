64d86833907e2096f273c220186b7265704cf104
ViniciusMazinetti <vinicius_mazinetti@hotmail.com>
2021-08-24 18:18:24 -0300

    Adicionada a tela de recuperação de senha

--- appScreens/components/elements/MyTabs.js
+++ appScreens/components/elements/MyTabs.js
@@ -2,11 +2,9 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from '../screens/Login';
-import Register from '../screens/Register';
-import CreateGarden from '../screens/CreateGarden';
 import Principal from '../screens/Principal';
 import Settings from '../screens/Settings';
+import Password from '../screens/Password';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,7 +13,7 @@ function MyTabs() {
     <NavigationContainer>
         <Tab.Navigator screenOptions={{tabBarActiveTintColor: '#000', tabBarInactiveTintColor:'#FEFEFE',tabBarStyle:{backgroundColor:'#08B662'}}}>
             <Tab.Screen name="Home" component={Principal} options={{title:'Home',tabBarBadgeStyle:{color:'#FEFEFE'}}}/>
-            <Tab.Screen name="Histórico" component={Home} />
+            <Tab.Screen name="Histórico" component={Password} />
             <Tab.Screen name="Configuração" component={Settings} />
             <Tab.Screen name="Sair" component={Home} />
         </Tab.Navigator>
---
+++ appScreens/components/screens/Password.js
@@ -0,0 +1,87 @@
+import React, {useState} from 'react'
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native'
+import {
+    Button,
+    Input
+} from 'react-native-elements'
+
+const Password = () => {
+    
+    let [email,setEmail] = useState("")
+    return(
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Recuperação de Senha</Text>
+            <View style={styles.containerContent}>
+                <Text style={styles.textStyle}>Para recuperar a senha, favor informar seu e-mail cadastrado e clicar no botão <Text style={{fontWeight:'bold'}}>'Enviar Email'</Text>, para que sua senha seja enviada!</Text>
+                <Input 
+                    label="Email" 
+                    labelStyle={styles.labelStyle} 
+                    inputStyle={styles.inputStyle}
+                    onChangeText={(value) => {
+                    setEmail(value);
+                }}/>
+                <Button title='Enviar Email' buttonStyle={styles.buttonStyle} containerStyle={styles.buttonContainer}/>
+                <Button title='Retornar ao Login' buttonStyle={styles.buttonStyle} containerStyle={styles.buttonContainer}/>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonContainer:{
+        width: 160,
+        margin: 28
+      },
+    
+      buttonStyle:{
+        backgroundColor:'#09894C', 
+        borderStyle:'solid', 
+        borderColor:'#FEFEFE', 
+        borderWidth:1
+      },
+
+      container:{
+        flex: 1,
+        backgroundColor: '#08B662'
+      },
+
+      inputStyle: {
+        borderRadius: 16,
+        backgroundColor: 'white',
+        color: "#000",
+      },
+    
+      labelStyle: {
+        fontSize: 20,
+        paddingLeft: 10,
+        color: "#000"
+      },
+
+      containerContent:{
+        flexDirection: 'column',
+        margin: 28,
+        justifyContent: 'center',
+        alignItems:'center'
+      },
+
+      title:{
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginLeft: 16,
+          marginTop:16
+
+      },
+
+      textStyle:{
+          marginBottom:28,
+          fontSize: 20    
+
+      }
+})
+
+export default Password
