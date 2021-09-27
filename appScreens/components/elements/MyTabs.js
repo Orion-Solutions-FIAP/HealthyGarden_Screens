2771e2f309fb06fa5a82556e0c6417ba32f99621
ViniciusMazinetti <vinicius_mazinetti@hotmail.com>
2021-09-26 21:21:03 -0300

    Tela do historico implementada

--- appScreens/components/elements/MyTabs.js
+++ appScreens/components/elements/MyTabs.js
@@ -3,7 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import Principal from '../screens/Principal';
 import Settings from '../screens/Settings';
-import Password from '../screens/Password';
+import Historic from '../screens/Historic';
 import Icon from 'react-native-vector-icons/Feather';
 
 const Tab = createBottomTabNavigator();
@@ -34,9 +34,9 @@ function MyTabs() {
             return <Icon name={iconName} size={size} color={color} />;
         }})}>
             <Tab.Screen name="Home" component={Principal} options={{title:'Home',tabBarBadgeStyle:{color:'#FEFEFE'}}} option={{headerShown: false}}/>
-            <Tab.Screen name="Histórico" component={Password} />
+            <Tab.Screen name="Histórico" component={Historic} />
             <Tab.Screen name="Configuração" component={Settings} />
-            <Tab.Screen name="Sair" component={Home} />
+            <Tab.Screen name="Sair" component={Home} options={{headerShown: false}}/>
         </Tab.Navigator>
   );
 }
